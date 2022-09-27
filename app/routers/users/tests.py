56fb98283a65dd5761ade8811e62b724d869b5dc
Sipan Margaryan <sipanm19@gmail.com>
2022-09-27 16:18:22 +0400

    sssssss

--- app/routers/users/tests.py
+++ app/routers/users/tests.py
@@ -16,12 +16,12 @@ def mock_get_value(self, user, content, update_type):
     return [0. for i in range(220)]
 
 
-# def test_users_vector_success(monkeypatch):
-#     data = {"user_id": 24, "content_id": 24, "update_type": "post"}
-#     monkeypatch.setattr(InterestVector, "get_updated_vector", mock_get_value)
-#     response = client.post('users/update-user-vector', data=json.dumps(data))
-#     assert response.status_code == 200
-#     assert len(response.json()["vector"]) == 220
+def test_users_vector_success(monkeypatch):
+    data = {"user_id": 24, "content_id": 24, "update_type": "post"}
+    monkeypatch.setattr(InterestVector, "get_updated_vector", mock_get_value)
+    response = client.post('users/update-user-vector', data=json.dumps(data))
+    assert response.status_code == 200
+    assert len(response.json()["vector"]) == 220
 
 
 def test_users_vector_error():
@@ -34,8 +34,7 @@ def test_users_vector_invalid_data():
     data = {"user_id": 4, "content_id": "invalid", "update_type": "post"}
     response = client.post('users/update-user-vector', data=json.dumps(data))
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    data = response.json()
-    assert data["detail"] == "User not found!"
+    assert response.json() == {"detail": "User not found!"}
 
 
 def test_generate_user_vector():
